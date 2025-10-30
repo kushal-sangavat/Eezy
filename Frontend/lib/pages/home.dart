af7802f284e7ac1c0e338630b86e4bdb2b6b99f8
SharadMangal <sharadmangal1010@gmail.com>
2025-10-30 15:34:31 +0530

    Add missing Font Awesome import in home.dart

--- Frontend/lib/pages/home.dart
+++ Frontend/lib/pages/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import '../components/header/header.dart';
 import '../components/service_bloc.dart';
 // import 'package:jklu_eezy/apicall/auth_utils.dart';
